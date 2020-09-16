0054e9e4e005c470a521e78a5405fb5873752638
Collins Koech <collinskoechck34@gmail.com>
2020-09-16 23:31:03 +0300

    update

---
+++ patternb.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class patternb {
+    public static void main(String []args){
+        int i,j,k,n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows to be printed:");
+        n = sc.nextInt();
+    }
+}
